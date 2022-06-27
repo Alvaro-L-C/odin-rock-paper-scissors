2a888666cc5233ba67f90ddebb71c31d5dca3556
blank <useridro8464@protonmail.com>
2022-06-27 23:59:58 +0200

    Completed project
    Completed indext.html and script.js to operate as requested.
    Added img folder, with images of the three possible choices,
    and style.css.

--- script.js
+++ script.js
@@ -1,54 +1,102 @@
+let computerScore = 0;
+let playerScore = 0;
+let result = '';
+let lastPlayerOption = '';
+
 function getComputerPlay() {
     let num = Math.floor(Math.random() * 3);
     switch(num){
         case 0:
-            return "ROCK";
+            return 'rock';
         case 1:
-            return "PAPER";
+            return 'paper';
         case 2:
-            return "SCISSORS";
+            return 'scissors';
     }
 }
-
-let computerScore = 0;
-let playerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection == computerSelection){
-        return `That was a draw! You both chose ${playerSelection}`;
+        result = 'draw';
     } else if (
-            (playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
-            (playerSelection == "PAPER" && computerSelection == "ROCK")    ||
-            (playerSelection == "SCISSORS" && computerSelection == "PAPER")) {
+            (playerSelection == 'rock' && computerSelection == 'scissors') ||
+            (playerSelection == 'paper' && computerSelection == 'rock')    ||
+            (playerSelection == 'scissors' && computerSelection == 'paper')) {
                 playerScore++;
-                return `You Win! ${playerSelection} beats ${computerSelection}`;
+                result = 'player'
             }
     else {
         computerScore++;
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
+        result = 'computer';
     }
 }
 
 
+function gameCompleted() {
+    return playerScore == 5 || computerScore == 5;
+}
 
+const playResult = document.getElementById('playResult');
+const playMessage = document.getElementById('playMessage');
+const computerScrID = document.getElementById('computerScore');
+const playerScrID = document.getElementById('playerScore');
+const playerRock = document.getElementById('rock');
+const playerPaper = document.getElementById('paper');
+const playerScissors = document.getElementById('scissors');
+const computerSelImage = document.getElementById('computerPlay');
+const restart = document.getElementById('restart');
 
-function game() {
-    for(let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper or Scissors", "Paper");
-        playerSelection = playerSelection.toUpperCase();
+playerPaper.addEventListener('click',  () => startRound('paper'));
+playerRock.addEventListener('click', () => startRound('rock'));
+playerScissors.addEventListener('click', () => startRound('scissors'));
+restart.addEventListener('click', () => reloadPage());
+
+function startRound(playerSelection){
+    if (!gameCompleted()){
         const computerSelection = getComputerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`${playerScore} - ${computerScore}`);
-        playRound();
+        playRound(playerSelection, computerSelection);
+        updateScoreboard(playerSelection, computerSelection);
+        changeImage(computerSelection);
+        addCSSClass(playerSelection);
     }
-    if(playerScore > computerScore){
-        console.log(`YOU WIN! Final score: ${playerScore} - ${computerScore}`);
-    } else if (playerScore < computerScore) {
-        console.log(`YOU LOSE! Final score: ${playerScore} - ${computerScore}`);
-    } else {        
-        console.log(`YOU TIE! Final score: ${playerScore} - ${computerScore}`);
+
+    if(gameCompleted() && restart.classList.contains('hidden')){
+        restart.classList.remove('hidden');
+    }
+}
+function updateScoreboard(playerSelection, computerSelection) {
+    if(result == 'draw') {
+        playResult.textContent = 'You Draw!'
+        playMessage.textContent = `You both chose ${playerSelection}`;
+    } else if(result == 'player'){
+        playerScrID.textContent = `Player: ${playerScore}`;
+        playResult.textContent = 'You Win!'
+        playMessage.textContent = `${playerSelection} beats ${computerSelection}`;
+    } else {
+        computerScrID.textContent = `Computer: ${computerScore}`;
+        playResult.textContent = 'You Lose!'
+        playMessage.textContent = `${computerSelection} beats ${playerSelection}`;
     }
 }
 
+function addCSSClass(playerSelect){
+    document.querySelector(`#${playerSelect} img`).style.border = '5px #191919 solid';
+    if(lastPlayerOption != '' && playerSelect != lastPlayerOption){
+        document.querySelector(`#${lastPlayerOption} img`).style.border = '5px solid transparent';
+    } else {
+        computerSelImage.style.border = '5px #191919 solid';
+    }
+    lastPlayerOption = playerSelect;
+}
 
-game();
+function changeImage(computerSelection) {
+    if(computerSelImage.alt == computerSelection) return;
+    else {
+        computerSelImage.alt = computerSelection;
+        computerSelImage.src = `./img/${computerSelection}.png`;
+    }
+}
+
+function reloadPage() {
+    window.location.reload(true);
+}
